ea7d375fdf59a5a64a2aff180996b3745beb7ced
Daniel Dedja <danieldedja2@gmail.com>
2026-02-18 18:44:27 +0100

    Initial project setup with Vite and basic structure

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import Hero from "./components/Hero";
+import Services from "./components/Services";
+import Gallery from "./components/Gallery";
+import About from "./components/About";
+import Location from "./components/Location";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <>
+      <Hero />
+      <Services />
+      <Gallery />
+      <About />
+      <Location />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
