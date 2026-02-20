9dd1f9c89371f1b9769aa33b7648a6d47e4cb524
Daniel Dedja <danieldedja2@gmail.com>
2026-02-20 20:56:26 +0100

    style: refine spaciny

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,6 +1,15 @@
 
 function Services() {
-  return <section id="services"><div className="container"><h2>Services</h2></div></section>;
+
+  return( 
+  <section id="services">
+  <div className="container">
+    <h2>Shërbimet Tona</h2>
+  </div>
+  
+  </section>
+  );
+
 }
 
 export default Services;
