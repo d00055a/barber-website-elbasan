6f8e1536feb6fbb3ed2ec6d086f74f698d9739a8
Daniel Dedja <danieldedja2@gmail.com>
2026-02-20 20:09:42 +0100

    feat:Create structure of Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,31 @@
 
 function Hero() {
-  return <section id="hero"><div className="container"><h1>Hero</h1></div></section>;
+
+  return ( 
+  <section id="hero">
+    <div className="container">
+
+      <h1>Berber Profesional në Elbasan</h1>
+      <p>Ofrojmë qethje moderne, skin fade dhe modelim mjekre me përkujdesje maksimale 
+         dhe stil të personalizuar për çdo klient.</p>
+
+      <div className="hero-buttons">
+        <a href="tel:+355697258558" className="btn-primary">
+          Telefono Tani</a>
+
+        <a href="https://wa.me/355697258558" target="_blank" 
+        rel="noopener noreferrer" className="btn-whatsapp"
+        >
+          WhatsApp
+        </a>
+      </div>
+
+
+    </div>
+      
+  </section>
+  );
+
 }
 
 export default Hero;
