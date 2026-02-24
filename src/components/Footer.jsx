65bb7c375f80916afd4b6746a4609f145ef83c82
Daniel Dedja <danieldedja2@gmail.com>
2026-02-24 01:03:24 +0100

    Import react-icons library for the TikTok logo and give the anchor tags social-link classname

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,5 @@
+import { Instagram } from "lucide-react";
+import { FaTiktok } from "react-icons/fa";
 import "../styles/footer.css";
 
 function Footer() {
@@ -6,28 +8,33 @@ function Footer() {
       <div className="container footer-content">
 
         <div className="footer-brand">
-          <h3>Barber Elbasan</h3>
-          <p>Stil, profesionalizëm dhe përkujdesje për çdo detaj.</p>
+          <h3>Barber Shop Rois</h3>
+          <p>🌍Jeta nuk është perfekte, por flokët tuaj mund të jenë🌍</p>
         </div>
 
         <div className="footer-contact">
-          <p>📞 +355 6X XXX XXXX</p>
-          <p>📍 Elbasan, Shqipëri</p>
+          <p>📞 +355 69 725 8558 </p>
+          <p>📍 Rruga Rinia, Elbasan, Shqipëri </p>
         </div>
 
         <div className="footer-social">
-          <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
-            Instagram
+          <a href="https://www.instagram.com/barber_shop_rois?igsh=MWM1YWQxNWs2dWxuNA==" target="_blank" rel="noopener noreferrer"
+             className="social-link">
+            <Instagram size={18} />
+            <span>Instagram</span>
           </a>
-          <a href="https://tiktok.com" target="_blank" rel="noopener noreferrer">
-            TikTok
+
+          <a href="https://www.tiktok.com/@barber_shop_rois?_r=1&_t=ZS-94AbVgqH0oq" target="_blank" rel="noopener noreferrer" 
+             className="social-link">
+            <FaTiktok size={18} />
+            <span>TikTok</span>
           </a>
         </div>
 
       </div>
 
       <div className="footer-bottom">
-        <p>© {new Date().getFullYear()} Barber Elbasan. Të gjitha të drejtat e rezervuara.</p>
+        <p>© {new Date().getFullYear()} Barber Shop Rois. Të gjitha të drejtat e rezervuara.</p>
       </div>
     </footer>
   );
